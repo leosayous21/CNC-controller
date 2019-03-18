53199addacefc544005f55c27f91ac3768972bd0
Super Sayous <leosayous@gmail.com>
2019-03-18 20:36:54 +0100

    feat: add lock

--- serial.js
+++ serial.js
@@ -11,8 +11,9 @@ var lock = false;
 
 serialPortController.on('data', function (data) {
   accumulator += data;
-  if (data.indexOf('\n\n') !== -1) {
-    console.log('accumulator', accumulator);
+  var lastCharacters = data.slice(-4).toString();
+  if (lastCharacters === 'ok\r\n' || lastCharacters === '>\r\n\n') {
+    console.log('data', accumulator);
     serialResponse.emit('data', accumulator);
     accumulator='';
     lock=false;
@@ -36,8 +37,15 @@ serialPortController.on('error', function(error){
 
 exports.serialPortController = serialPortController;
 exports.serialResponse = serialResponse;
-exports.write=function(data){
+const write = function(data){
+  if(lock) {
+    console.log('locked - previous not finished');
+    setTimeout(() => write(data), 100);
+    return;
+  }
   lock=true;
   serialPortController.write(data);
 };
+exports.write=write;
 exports.locked=lock;
+setInterval(() => serialPortController.write('\n'), 2000); // in case of hard block
